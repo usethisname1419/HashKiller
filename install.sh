f1b9dbe18fb18aac86714799895fd4fc8ef60acc
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2023-10-01 12:26:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,15 @@ then
     exit 1
 fi
 
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 is not available. Please install pip for Python 3 and try again."
+    exit 1
+fi
+
+pip3 install -r requirements.txt
+
+
 
 chmod +x hashkiller.py
 
