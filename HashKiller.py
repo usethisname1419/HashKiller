26b271cdb9c89ccedcffe5d92d61a09325e949fd
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2023-11-07 12:47:08 -0600

    Update HashKiller.py

--- HashKiller.py
+++ HashKiller.py
@@ -8,60 +8,45 @@ import time
 from passlib.hash import sha512_crypt, nthash
 import threading
 from colorama import Fore, init
+
 print_lock = threading.Lock()
 init(autoreset=True)
 
 shutdown_event = threading.Event()
-
-
+match_found_event = threading.Event()
 def generate_combinations(chars, min_length, max_length=22):
     for length in range(min_length, max_length + 1):
         for combo in product(chars, repeat=length):
             yield ''.join(combo)
 
-
 def md5_hash(string):
     return hashlib.md5(string.encode()).hexdigest()
-
 
 def sha1_hash(string):
     return hashlib.sha1(string.encode()).hexdigest()
 
-
 def sha256_hash(string):
     return hashlib.sha256(string.encode()).hexdigest()
-
 
 def sha512_unix_hash(string):
     return sha512_crypt.using(rounds=5000).hash(string)
 
-
 def nt_hash(string):
     return nthash.hash(string)
 
+def print_current_password(hash_type, target_hash, current_word):
+    with print_lock:
+        print(f'\rTYPE: {hash_type} | TARGET: {target_hash} | TRYING: {current_word}', end='')
+        print("\033[K", end='', flush=True)
 
-def resource_usage():
-    cpu_percent = psutil.cpu_percent(interval=0)  
-    memory_info = psutil.virtual_memory()
-    return cpu_percent, memory_info.percent
-
-def print_current_password(hash_type, target_hash, current_word, counter):
-
-    if counter % 10000 == 0:
-        with print_lock:
-            cpu, mem = resource_usage()
-            print(f'\rTYPE: {hash_type} | TARGET: {target_hash} | TRYING: {current_word}', end='')
-            print(f'\n\rCPU Usage: {cpu}%    Memory Usage: {mem}%', end='')
-            print("\033[F", end='', flush=True)
-
-
-def brute_force(target_hash, hash_type, chars, min_length, max_length, safety_pause=None):
+def brute_force(target_hash, hash_type, chars, min_length, max_length, success_event, safety_pause=None):
     counter = 0
+
     for word in generate_combinations(chars, min_length, max_length):
-        if shutdown_event.is_set():
+        if shutdown_event.is_set() or success_event.is_set(): 
             return
 
-        print_current_password(hash_type, target_hash, word, counter)
+        print_current_password(hash_type, target_hash, word)
 
         if hash_type == 'md5':
             computed_hash = md5_hash(word)
@@ -71,6 +56,10 @@ def brute_force(target_hash, hash_type, chars, min_length, max_length, safety_pa
             computed_hash = sha256_hash(word)
         elif hash_type == 'sha512_unix':
             if sha512_crypt.verify(word, target_hash):
+                success_event.set()  
+                with print_lock:
+                    print(f"\nMatch found for hash {target_hash}:{Fore.LIGHTBLUE_EX} {word}")
+                    time.sleep(2)
                 return word
         elif hash_type == 'nt':
             computed_hash = nt_hash(word)
@@ -78,24 +67,62 @@ def brute_force(target_hash, hash_type, chars, min_length, max_length, safety_pa
             raise ValueError("Unsupported hash type")
 
         if computed_hash == target_hash:
+            success_event.set()
+            match_found_event.set()
+            with print_lock:
+                print(f"\nMatch found for hash {target_hash}:{Fore.LIGHTBLUE_EX} {word}")
             return word
 
         counter += 1
-        if args.safety:
-            if args.safety == 1 and counter % 699999 == 0:
+        if safety_pause:
+            if safety_pause == 1 and counter % 699999 == 0:
                 time.sleep(1.35)
-            elif args.safety == 2 and counter % 699999 == 0:
+            elif safety_pause == 2 and counter % 699999 == 0:
                 time.sleep(2)
-            elif args.safety == 3 and counter % 199999 == 0:
+            elif safety_pause == 3 and counter % 199999 == 0:
                 time.sleep(1.5)
 
 
-def worker(target_hash, hash_type, chars, min_length, max_length, safety_pause, results):
+def resource_printer():
+    while not shutdown_event.is_set():
+        cpu, mem = resource_usage()
+        with print_lock:
+
+            print(f'\n\rCPU Usage: {cpu}%    Memory Usage: {mem}%', end='', flush=True)
+            print("\033[F", end='', flush=True)
+        time.sleep(1)
+        if match_found_event.is_set():
+            break
+def resource_usage():
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory_info = psutil.virtual_memory()
+    return cpu_percent, memory_info.percent
+
+def crack_hash(target_hash, hash_type, chars, min_length, max_length, safety_pause=None):
+    results = []
+    threads = []
     try:
-        res = brute_force(target_hash, hash_type, chars, min_length, max_length, safety_pause)
-        results.append(res)
-    except Exception as e:
-        print(f"Error in worker thread: {e}")
+        for _ in range(args.threads):
+            t = threading.Thread(target=brute_force,
+                                 args=(target_hash, hash_type, chars, min_length, max_length, safety_pause))
+            t.start()
+            threads.append(t)
+
+        for t in threads:
+            t.join()
+
+    except KeyboardInterrupt:
+        print("\nInitiating graceful shutdown. Please wait...")
+        shutdown_event.set()
+        for t in threads:
+            t.join()
+
+    finally:
+        if args.threads > 1:
+            t_printer.join()
+
+    results.append(brute_force(target_hash, args.hash_type, chars, min_length, 22, safety_pause))
+    return results
 
 
 if __name__ == "__main__":
@@ -121,6 +148,7 @@ if __name__ == "__main__":
     try:
         with open(args.hash, 'r') as file:
             target_hash = file.readline().strip()
+
     except FileNotFoundError:
         print("Error: Specified file not found.")
         exit(1)
@@ -133,24 +161,37 @@ if __name__ == "__main__":
         exit(1)
 
     try:
-        results = []
-        threads = []
+        success_event = threading.Event()  #success event to signal threads to stop
+
+        threads = []  
+
         for _ in range(args.threads):
-            t = threading.Thread(target=worker,
-                                 args=(target_hash, args.hash_type, chars, min_length, 22, safety_pause, results))
+            t = threading.Thread(target=brute_force,
+                                 args=(target_hash, args.hash_type, chars, min_length, 22, success_event, safety_pause))
             t.start()
             threads.append(t)
 
+        if args.threads > 1:
+            t_printer = threading.Thread(target=resource_printer)
+            t_printer.start()
+
         for t in threads:
             t.join()
+
+        if args.threads > 1:
+            t_printer.join()
+
+        results = brute_force(target_hash, args.hash_type, chars, min_length, 22, success_event, safety_pause)
+        if results:
+            print(f"\nMatch found for hash {target_hash}:{Fore.LIGHTBLUE_EX} {results}")
 
     except KeyboardInterrupt:
         print("\nInitiating graceful shutdown. Please wait...")
-        shutdown_event.set()
+        success_event.set()  #success event to stop threads
         for t in threads:
             t.join()
+        if args.threads > 1:
+            t_printer.join()
 
-    if results and results[0]:
-        print(f"\nMatch found for hash {target_hash}:{Fore.LIGHTBLUE_EX} {results[0]}")
-    else:
+    if not success_event.is_set():
         print(f"\nNo match found for hash {target_hash}.")
